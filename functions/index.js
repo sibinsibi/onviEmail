9ea92114a7a39ad7cd6cd268912c0691403d98db
sibin <asibin99@gmail.com>
2021-03-18 12:34:35 +0000

    finished

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,61 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
-admin.initializeApp();
+const {Firestore} = require('@google-cloud/firestore');
+const firestore = new Firestore();
 
-
-
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   console.log('sibin sss')
-//   response.send("Hello from Firebase!");
-// });
-
-exports.sentEmail = functions.firestore.document('ClientSignUp/{id}').onCreate((snap, context) => {
-      const newValue = snap.data();
-        console.log(context)
+admin.initializeApp({
+  storageBucket: "alpha-media-dev.appspot.com"
 });
+
+exports.sentEmail = functions.firestore.document('ClientSignUp/{id}').onCreate(async (snap, context) => {
+      
+      const newValue = snap.data();
+      const id = context.params.id;
+      //let id = 'p0idBa2nmu3rIip7eri5'
+      
+      const adharUrl = await admin.storage().bucket().file(`ClientSignUp/${id}/${newValue.Adhar}`).getSignedUrl({action: 'read', expires: '03-09-3000'})
+      const panUrl = await admin.storage().bucket().file(`ClientSignUp/${id}/${newValue.Pan}`).getSignedUrl({action: 'read', expires: '03-09-3000'})
+      const chequeUrl = await admin.storage().bucket().file(`ClientSignUp/${id}/${newValue.CancelledCheque}`).getSignedUrl({action: 'read', expires: '03-09-3000'})
+      const gstUrl = await admin.storage().bucket().file(`ClientSignUp/${id}/${newValue.Gst}`).getSignedUrl({action: 'read', expires: '03-09-3000'})
+
+      let emailObj = {
+        Name: `Name: ${newValue.Name}`,
+        CompanyName: `Company Name: ${newValue.CompanyName}`,
+        Mobile: `Mobile: ${newValue.Mobile}`,
+        Email: `Email: ${newValue.Email}`,
+        AccNo: `Account No: ${newValue.AccNo}`,
+        AccName: `Account Name: ${newValue.AccName}`,
+        Branch: `Branch: ${newValue.Branch}`,
+        Ifsc: `IFSC: ${newValue.Ifsc}`,
+        Adhar: `Adhar: ${newValue.Adhar}`,
+        Pan: `Pan: ${newValue.Pan}`,
+        CancelledCheque: `Cancelled Cheque: ${newValue.CancelledCheque}`,
+        Gst: `Gst: ${newValue.Gst}`,
+        AdharDwldUrl: adharUrl[0],
+        PanDwldUrl: panUrl[0],
+        CancelledChequeDwldUrl: chequeUrl[0],
+        GstDwldUrl: gstUrl[0],
+      }
+
+      await firestore.collection('ClientSignUp').doc(id).update({
+        AdharDwldUrl: adharUrl[0],
+        PanDwldUrl: panUrl[0],
+        CancelledChequeDwldUrl: chequeUrl[0],
+        GstDwldUrl: gstUrl[0],
+      })
+});
+
+// {
+//   Name: 'Sibin',
+//   CompanyName: 'ONVI',
+//   Mobile: '9562571150',
+//   Email: 'ss@ss.com',
+//   AccNo: '9562588552',
+//   AccName: 'Test',
+//   Branch: 'Chennai',
+//   Ifsc: 'LOLOL',
+//   Adhar: 'adhar.jpg',
+//   Pan: 'pan.jpg',
+//   CancelledCheque: 'chequ.jpg',
+//   Gst: 'gst.jpg'
+// }
